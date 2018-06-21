da27f503a27541fc43f196ef64680a0bf1ca2f06
alexskrn <askrn123@gmail.com>
2018-06-22 00:09:22 +0300

    Implemented create function, create template

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor
 
 app = Flask(__name__)
 
@@ -15,9 +15,28 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+
+@app.route('/create/', methods=['GET', 'POST'])
+def create():
+    """Handle creation of donors and donations."""
+    # If user types an existing name, add a donation for such name
+    # If user types a new name, create a new donor
+    if request.method == 'POST':
+        donor = request.form['name']
+        amount = int(request.form['number'])
+        query = Donor.select().where(Donor.name == donor)
+        if query.exists():
+            Donation(donor=query.get(), value=amount).save()
+            return redirect(url_for('all'))
+        else:
+            new_donor = Donor(name=donor)
+            new_donor.save()
+            Donation(donor=new_donor, value=amount).save()
+            return redirect(url_for('all'))
+    return render_template('create.jinja2')
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
-
